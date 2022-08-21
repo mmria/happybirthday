4f6be270f37a1e02d2cfe61af7df3e912f679984
geoshavel <geoshavel@gmail.com>
2022-08-22 00:24:25 +0300

    deploy

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
 import Layout from 'layout/Layout';
+import { documentReady, pageLoad } from 'utils';
+import splitTextInit from './components/splitTextInit';
 
 export default class App {
 	constructor() {
@@ -13,27 +15,47 @@ export default class App {
 		this.init();
 	}
 
-	importPage() {
-		if (this.pageClass && this.pageClass !== null) {
-			import(`./pages/${this.pageClass}`)
-				.then(({ default: pageClass }) => {
-					const newPage = new pageClass();
-					newPage.init();
-				})
-				.catch((error) => {
-					console.error(
-						'Failed to load page, check data-template-name at root if correct',
-					);
-					console.dir(error, error.stack);
-				});
-		}
+	loadFunc() {
+		if (window.innerWidth < 1200) window.alert('Мобилной версии пока нет :( Зайди с пк');
+
+		document.body.classList.add('loaded_state');
+
+		const audioWrap = document.querySelector('.audio');
+		const audio = document.createElement('video');
+
+		const btn = document.querySelector('.button');
+		const input = document.querySelector('.input');
+
+		audio.type = 'video/mp4';
+		audio.src = 'images/stay.mp4';
+		audio.loop = true;
+		audioWrap.appendChild(audio);
+
+		splitTextInit();
+
+		btn.addEventListener('click', () => {
+			if (input.value === '1234') {
+				document.body.classList.add('auth_state');
+
+				setTimeout(() => {
+					audio.play();
+					audio.volume = 0.15;
+				}, 300);
+			}
+		});
+
+		window.addEventListener('resize', () => {
+			// eslint-disable-next-line no-self-assign
+			window.location = window.location;
+		});
 	}
 
 	init() {
 		const initLayout = new Layout();
 		// scrollProxy();
-		setTimeout(() => {
-			this.importPage();
-		}, 0);
+
+		pageLoad(() => {
+			this.loadFunc();
+		});
 	}
 }
--- src/js/pages/IndexPage.js
+++ src/js/pages/IndexPage.js
@@ -1,6 +1,5 @@
 import { GLOBAL_VARS } from 'utils/constants';
 import { documentReady, pageLoad } from 'utils';
-import splitTextInit from '../components/splitTextInit';
 
 export default class IndexPage {
 	constructor() {
@@ -8,33 +7,6 @@ export default class IndexPage {
 	}
 
 	loadFunc() {
-		if (window.innerWidth < 1200) window.alert('Мобилной версии пока нет :( Зайди с пк');
-
-		document.body.classList.add('loaded_state');
-
-		const audioWrap = document.querySelector('.audio');
-		const audio = document.createElement('video');
-
-		const btn = document.querySelector('.button');
-		const input = document.querySelector('.input');
-
-		audio.type = 'video/mp4';
-		audio.src = 'images/stay.mp4';
-		audio.loop = true;
-		audioWrap.appendChild(audio);
-
-		splitTextInit();
-
-		btn.addEventListener('click', () => {
-			if (input.value === '1234') {
-				document.body.classList.add('auth_state');
-
-				setTimeout(() => {
-					audio.play();
-					audio.volume = 0.15;
-				}, 300);
-			}
-		});
 	}
 
 	init() {
